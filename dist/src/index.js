918f8cbf0dbfb392b7940f640f618e82e5825131
Moyses <moisesemilianohernandez0@gmail.com>
2023-06-23 13:48:24 -0600

    Daga con ballesta version 3

--- dist/src/index.js
+++ dist/src/index.js
@@ -63,58 +63,242 @@ function incrDistFunc() {
 function decrDistFunc() {
     vp(0, 0, 0.5);
 }
-function pza1DerFunc() {
-    var af = 10;
-    Rota3D.initRotate(obj.w[139], obj.w[140], af * Math.PI / 180);
-    for (var i = 201; i <= 238; i++) {
-        obj.w[i] = Rota3D.rotate(obj.w[i]);
+/* function pza1DerFunc() {
+  let af = 10;
+    
+    Rota3D.initRotate( obj.w[139], obj.w[140], af*Math.PI/180);
+    
+  for (let i = 201; i <= 238; i++){
+    obj.w[i] = Rota3D.rotate(obj.w[i]);
     }
     cv.setObj(obj);
-    cv.paint();
+  cv.paint();
 }
+
 function pza1IzqFunc() {
-    var af = -10;
-    Rota3D.initRotate(obj.w[139], obj.w[140], af * Math.PI / 180);
-    for (var i = 201; i <= 238; i++) {
-        obj.w[i] = Rota3D.rotate(obj.w[i]);
+  let af = -10;
+    
+    Rota3D.initRotate( obj.w[139], obj.w[140], af*Math.PI/180);
+    
+  for (let i = 201; i <= 238; i++){
+    obj.w[i] = Rota3D.rotate(obj.w[i]);
     }
     cv.setObj(obj);
-    cv.paint();
+  cv.paint();
 }
 function pza12DerFunc() {
-    var af = 10;
-    console.log(obj.w[29], obj.w[30], obj.w[6]);
-    Rota3D.initRotate(obj.w[29], obj.w[30], af * Math.PI / 180);
-    for (var i = 101; i <= 140; i++) {
-        obj.w[i] = Rota3D.rotate(obj.w[i]);
-    }
-    for (var i = 201; i <= 238; i++) {
-        obj.w[i] = Rota3D.rotate(obj.w[i]);
+  let af = 10;
+  console.log(obj.w[29], obj.w[30], obj.w[6]);
+  Rota3D.initRotate(obj.w[29], obj.w[30], af * Math.PI / 180);
+    
+  for (let i = 101; i <= 140; i++){
+    obj.w[i] = Rota3D.rotate(obj.w[i]);
+  }
+  for (let i = 201; i <= 238; i++){
+    obj.w[i] = Rota3D.rotate(obj.w[i]);
     }
     cv.setObj(obj);
-    cv.paint();
+  cv.paint();
 }
+
 function pza12IzqFunc() {
-    var af = -10;
-    console.log(obj.w[29], obj.w[30]);
-    Rota3D.initRotate(obj.w[29], obj.w[30], af * Math.PI / 180);
-    for (var i = 101; i <= 140; i++) {
+  let af = -10;
+  console.log(obj.w[29], obj.w[30]);
+    Rota3D.initRotate( obj.w[29], obj.w[30], af*Math.PI/180);
+    
+  for (let i = 101; i <= 140; i++){
+    obj.w[i] = Rota3D.rotate(obj.w[i]);
+    }
+  for (let i = 201; i <= 238; i++){
+    obj.w[i] = Rota3D.rotate(obj.w[i]);
+    }
+  
+    cv.setObj(obj);
+  cv.paint();
+}
+
+function Patitafunc(){
+ let af = 20;
+ Rota3D.initRotate( obj.w[101], obj.w[102], af*Math.PI/180);
+  for(let i =1 ; i <= 8; i++){
+    obj.w[i] = Rota3D.rotate(obj.w[i]);
+  }
+
+  cv.setObj(obj);
+  cv.paint();
+} */
+var anima;
+var audio = document.getElementById("Desenvainar");
+var audio2 = document.getElementById("efectoBallesta");
+function girar(angulo, ancla1, ancla2, inicio, final) {
+    Rota3D.initRotate(obj.w[ancla1], obj.w[ancla2], angulo * Math.PI / 180);
+    for (var i = inicio; i <= final; i++) {
         obj.w[i] = Rota3D.rotate(obj.w[i]);
     }
-    for (var i = 201; i <= 238; i++) {
-        obj.w[i] = Rota3D.rotate(obj.w[i]);
+}
+function metHoja() {
+    for (var i = 1; i <= 6; i++) {
+        if (obj.w[1].x <= -1) {
+            obj.w[i].x = obj.w[i].x + 0.5;
+        }
+        else {
+            clearInterval(anima);
+        }
+    }
+    if (obj.w[1].x <= 1.5) {
+        girar(-1, 1200, 1201, 141, 144);
+        girar(1, 1200, 1201, 145, 148);
+        girar(-1, 1202, 1203, 149, 152);
+        girar(1, 1202, 1203, 153, 156);
+    }
+    else {
+        clearInterval(anima);
+    }
+    if (obj.w[1].x <= 0.5) {
+        for (var i = 149; i <= 156; i++) {
+            obj.w[i].x = obj.w[i].x + 0.01;
+        }
     }
     cv.setObj(obj);
     cv.paint();
 }
-function Patitafunc() {
-    var af = 20;
-    Rota3D.initRotate(obj.w[101], obj.w[102], af * Math.PI / 180);
-    for (var i = 1; i <= 8; i++) {
-        obj.w[i] = Rota3D.rotate(obj.w[i]);
+function sacHoja() {
+    for (var i = 1; i <= 6; i++) {
+        if (obj.w[1].x >= -5) {
+            obj.w[i].x = obj.w[i].x - 0.5;
+        }
+        else {
+            clearInterval(anima);
+        }
+    }
+    if (obj.w[1].x <= 1.5) {
+        girar(1, 1200, 1201, 141, 144);
+        girar(-1, 1200, 1201, 145, 148);
+        girar(1, 1202, 1203, 149, 152);
+        girar(-1, 1202, 1203, 153, 156);
+    }
+    else {
+        clearInterval(anima);
+    }
+    if (obj.w[1].x <= 0.5) {
+        for (var i = 149; i <= 156; i++) {
+            obj.w[i].x = obj.w[i].x - 0.01;
+        }
     }
     cv.setObj(obj);
     cv.paint();
+}
+function saveBallesta() {
+    if (obj.w[207].z >= 1.7) {
+        girar(-10, 1208, 1209, 205, 210);
+        girar(10, 1210, 1211, 211, 216);
+    }
+    else {
+        clearInterval(anima);
+    }
+    cv.setObj(obj);
+    cv.paint();
+}
+function desplagarBallesta() {
+    if (obj.w[207].z <= 2.1) {
+        girar(10, 1208, 1209, 205, 210);
+        girar(-10, 1210, 1211, 211, 216);
+    }
+    else {
+        clearInterval(anima);
+    }
+    cv.setObj(obj);
+    cv.paint();
+}
+function abirBrazalete() {
+    if (obj.w[14].y <= 1.35) {
+        girar(3, 1204, 1205, 14, 14);
+        girar(3, 1204, 1205, 46, 46);
+        girar(3, 1204, 1205, 185, 185);
+        girar(3, 1204, 1205, 157, 157);
+        girar(3, 1204, 1205, 22, 22);
+        girar(3, 1204, 1205, 158, 158);
+        girar(3, 1204, 1205, 54, 54);
+        girar(3, 1204, 1205, 186, 186);
+        girar(-3, 1206, 1207, 183, 183);
+        girar(-3, 1206, 1207, 15, 15);
+        girar(-3, 1206, 1207, 187, 187);
+        girar(-3, 1206, 1207, 47, 47);
+        girar(-3, 1206, 1207, 184, 184);
+        girar(-3, 1206, 1207, 23, 23);
+        girar(-3, 1206, 1207, 188, 188);
+        girar(-3, 1206, 1207, 55, 55);
+    }
+    else {
+        clearInterval(anima);
+    }
+    cv.setObj(obj);
+    cv.paint();
+}
+function cerrarBrazalete() {
+    if (obj.w[14].y >= 1) {
+        girar(-3, 1204, 1205, 14, 14);
+        girar(-3, 1204, 1205, 46, 46);
+        girar(-3, 1204, 1205, 185, 185);
+        girar(-3, 1204, 1205, 157, 157);
+        girar(-3, 1204, 1205, 22, 22);
+        girar(-3, 1204, 1205, 158, 158);
+        girar(-3, 1204, 1205, 54, 54);
+        girar(-3, 1204, 1205, 186, 186);
+        girar(3, 1206, 1207, 183, 183);
+        girar(3, 1206, 1207, 15, 15);
+        girar(3, 1206, 1207, 187, 187);
+        girar(3, 1206, 1207, 47, 47);
+        girar(3, 1206, 1207, 184, 184);
+        girar(3, 1206, 1207, 23, 23);
+        girar(3, 1206, 1207, 188, 188);
+        girar(3, 1206, 1207, 55, 55);
+    }
+    else {
+        clearInterval(anima);
+    }
+    cv.setObj(obj);
+    cv.paint();
+}
+function intHoja() {
+    metHoja();
+}
+function outHoja() {
+    sacHoja();
+}
+function svBallesta() {
+    saveBallesta();
+}
+function despBallesta() {
+    desplagarBallesta();
+}
+function openBrazalete() {
+    abirBrazalete();
+}
+function closeBrazalete() {
+    cerrarBrazalete();
+}
+function meterHoja() {
+    anima = setInterval(intHoja, 25);
+    audio.play();
+}
+function sacarHoja() {
+    audio.play();
+    anima = setInterval(outHoja, 25);
+}
+function gBallesta() {
+    audio2.play();
+    anima = setInterval(svBallesta, 25);
+}
+function outBallesta() {
+    audio2.play();
+    anima = setInterval(despBallesta, 25);
+}
+function Abrir() {
+    anima = setInterval(openBrazalete, 25);
+}
+function Cerrar() {
+    anima = setInterval(closeBrazalete, 25);
 }
 document.getElementById('file-input').addEventListener('change', leerArchivo, false);
 document.getElementById('eyeDown').addEventListener('click', eyeDownFunc, false);
@@ -124,11 +308,17 @@ document.getElementById('eyeRight').addEventListener('click', eyeRightFunc, fals
 document.getElementById('incrDist').addEventListener('click', incrDistFunc, false);
 document.getElementById('decrDist').addEventListener('click', decrDistFunc, false);
 //movimiento de piezas
-document.getElementById('pza1Izq').addEventListener('click', pza1IzqFunc, false);
+/* document.getElementById('pza1Izq').addEventListener('click', pza1IzqFunc, false);
 document.getElementById('pza1Der').addEventListener('click', pza1DerFunc, false);
 document.getElementById('pza12Izq').addEventListener('click', pza12IzqFunc, false);
 document.getElementById('pza12Der').addEventListener('click', pza12DerFunc, false);
-document.getElementById('Patita').addEventListener('click', Patitafunc, false);
+document.getElementById('Patita').addEventListener('click', Patitafunc, false); */
+document.getElementById('Cerrar').addEventListener('click', Cerrar, false);
+document.getElementById('Abrir').addEventListener('click', Abrir, false);
+document.getElementById('Desplegar-Ballesta').addEventListener('click', outBallesta, false);
+document.getElementById('Guardar-Ballesta').addEventListener('click', gBallesta, false);
+document.getElementById('Sacar-Hoja').addEventListener('click', sacarHoja, false);
+document.getElementById('Meter-Hoja').addEventListener('click', meterHoja, false);
 var Pix, Piy;
 var Pfx, Pfy;
 var theta = 0.3, phi = 1.3, SensibilidadX = 0.02, SensibilidadY = 0.02;
--- src/index.ts
+++ src/index.ts
@@ -78,7 +78,7 @@ function decrDistFunc() {
   vp(0, 0, 0.5);
 }
 
-function pza1DerFunc() {
+/* function pza1DerFunc() {
   let af = 10;
  	
 	Rota3D.initRotate( obj.w[139], obj.w[140], af*Math.PI/180);	
@@ -141,8 +141,178 @@ function Patitafunc(){
 
   cv.setObj(obj);
   cv.paint();
+} */
+let anima
+let audio=document.getElementById("Desenvainar") as HTMLAudioElement
+let audio2=document.getElementById("efectoBallesta") as HTMLAudioElement
+function girar(angulo,ancla1,ancla2,inicio,final){  
+  Rota3D.initRotate( obj.w[ancla1], obj.w[ancla2], angulo*Math.PI/180);
+   for(let i =inicio ; i <= final; i++){
+     obj.w[i] = Rota3D.rotate(obj.w[i]);
+   }
 }
+function metHoja(){  
+  for (let i = 1; i <= 6; i++){
+    if(obj.w[1].x<=-1){
+      obj.w[i].x=obj.w[i].x+0.5
+    }else{
+      clearInterval(anima)
+    }
+	}
+  if(obj.w[1].x<=1.5){
+    girar(-1,1200,1201,141,144);
+    girar(1,1200,1201,145,148);
+    girar(-1,1202,1203,149,152);
+    girar(1,1202,1203,153,156);
+  }else{
+    clearInterval(anima)
+  }
+  if(obj.w[1].x<=0.5){
+    for(let i = 149; i <= 156; i++){
+      obj.w[i].x=obj.w[i].x+0.01
+    }
+  }
+  cv.setObj(obj);
+  cv.paint();  
+}
+function sacHoja(){
+  for (let i = 1; i <= 6; i++){
+    if(obj.w[1].x>=-5){
+      obj.w[i].x=obj.w[i].x-0.5
+    }else{
+      clearInterval(anima)
+    }
+	}
+  if(obj.w[1].x<=1.5){
+    girar(1,1200,1201,141,144);
+    girar(-1,1200,1201,145,148);
+    girar(1,1202,1203,149,152);
+    girar(-1,1202,1203,153,156);
+  }else{
+    clearInterval(anima)
+  }
+  if(obj.w[1].x<=0.5){
+    for(let i = 149; i <= 156; i++){
+      obj.w[i].x=obj.w[i].x-0.01
+    }
+  }  
+  cv.setObj(obj);
+  cv.paint();
+}
+function saveBallesta(){    
+  if(obj.w[207].z>=1.7){
+    girar(-10,1208,1209,205,210)
+    girar(10,1210,1211,211,216)
+  }else{
+    clearInterval(anima)
+  }  
+  cv.setObj(obj);
+  cv.paint();
+}
+function desplagarBallesta(){    
+  if(obj.w[207].z<=2.1){
+    girar(10,1208,1209,205,210)
+    girar(-10,1210,1211,211,216)
+  }else{
+    clearInterval(anima)
+  }  
+  cv.setObj(obj);
+  cv.paint();
+}
+function abirBrazalete(){    
+  if(obj.w[14].y<=1.35){
+    girar(3,1204,1205,14,14)
+    girar(3,1204,1205,46,46)
+    girar(3,1204,1205,185,185)
+    girar(3,1204,1205,157,157)
+    girar(3,1204,1205,22,22)
+    girar(3,1204,1205,158,158)
+    girar(3,1204,1205,54,54)
+    girar(3,1204,1205,186,186)
+    
 
+    girar(-3,1206,1207,183,183)
+    girar(-3,1206,1207,15,15)
+    girar(-3,1206,1207,187,187)    
+    girar(-3,1206,1207,47,47)    
+    girar(-3,1206,1207,184,184)    
+    girar(-3,1206,1207,23,23)    
+    girar(-3,1206,1207,188,188)    
+    girar(-3,1206,1207,55,55)    
+    
+  }else{
+    clearInterval(anima)
+  }  
+  cv.setObj(obj);
+  cv.paint();
+}
+function cerrarBrazalete(){    
+  if(obj.w[14].y>=1){
+    girar(-3,1204,1205,14,14)
+    girar(-3,1204,1205,46,46)
+    girar(-3,1204,1205,185,185)
+    girar(-3,1204,1205,157,157)
+    girar(-3,1204,1205,22,22)
+    girar(-3,1204,1205,158,158)
+    girar(-3,1204,1205,54,54)
+    girar(-3,1204,1205,186,186)
+    
+
+    girar(3,1206,1207,183,183)
+    girar(3,1206,1207,15,15)
+    girar(3,1206,1207,187,187)    
+    girar(3,1206,1207,47,47)    
+    girar(3,1206,1207,184,184)    
+    girar(3,1206,1207,23,23)    
+    girar(3,1206,1207,188,188)    
+    girar(3,1206,1207,55,55)    
+    
+  }else{
+    clearInterval(anima)
+  }  
+  cv.setObj(obj);
+  cv.paint();
+}
+function intHoja(){
+  metHoja()
+}
+function outHoja(){
+  sacHoja()
+}
+function svBallesta(){
+  saveBallesta()
+}
+function despBallesta(){
+  desplagarBallesta()
+}
+function openBrazalete(){
+  abirBrazalete()
+}
+function closeBrazalete(){
+  cerrarBrazalete()
+}
+function meterHoja(){
+  anima=setInterval(intHoja,25)
+  audio.play()
+}
+function sacarHoja(){
+  audio.play()
+  anima=setInterval(outHoja,25)
+}
+function gBallesta(){
+  audio2.play()
+  anima=setInterval(svBallesta,25)
+}
+function outBallesta(){
+  audio2.play()
+  anima=setInterval(despBallesta,25)
+}
+function Abrir(){
+  anima=setInterval(openBrazalete,25)
+}
+function Cerrar(){
+  anima=setInterval(closeBrazalete,25)
+}
 document.getElementById('file-input').addEventListener('change', leerArchivo, false);
 document.getElementById('eyeDown').addEventListener('click', eyeDownFunc, false);
 document.getElementById('eyeUp').addEventListener('click', eyeUpFunc, false);
@@ -154,11 +324,17 @@ document.getElementById('decrDist').addEventListener('click', decrDistFunc, fals
 
 
 //movimiento de piezas
-document.getElementById('pza1Izq').addEventListener('click', pza1IzqFunc, false);
+/* document.getElementById('pza1Izq').addEventListener('click', pza1IzqFunc, false);
 document.getElementById('pza1Der').addEventListener('click', pza1DerFunc, false);
 document.getElementById('pza12Izq').addEventListener('click', pza12IzqFunc, false);
 document.getElementById('pza12Der').addEventListener('click', pza12DerFunc, false);
-document.getElementById('Patita').addEventListener('click', Patitafunc, false);
+document.getElementById('Patita').addEventListener('click', Patitafunc, false); */
+document.getElementById('Cerrar').addEventListener('click', Cerrar, false);
+document.getElementById('Abrir').addEventListener('click', Abrir, false);
+document.getElementById('Desplegar-Ballesta').addEventListener('click', outBallesta , false);
+document.getElementById('Guardar-Ballesta').addEventListener('click', gBallesta, false);
+document.getElementById('Sacar-Hoja').addEventListener('click', sacarHoja, false);
+document.getElementById('Meter-Hoja').addEventListener('click', meterHoja , false);
 
 let Pix: number, Piy: number;
 let Pfx: number, Pfy: number;
